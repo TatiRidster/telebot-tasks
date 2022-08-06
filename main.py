1dca63d1704003a0cb1aaddf4903bba407dbf53c
po100lit <po100lit@mail.ru>
2022-08-06 20:35:48 +0600

    добавил вывод всех дел

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from telegram import Update, Bot
+from telegram import Update, Bot, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 import os
+from controllers import *
 
 
 def tasks_bot(token):
@@ -11,10 +12,20 @@ def tasks_bot(token):
 
     def start(update, context):
         arg = context.args
+        print(arg)
+        keyboard = ReplyKeyboardMarkup([], resize_keyboard=True)
+        item_1 = KeyboardButton('Посмотреть')
+        item_2 = KeyboardButton('Добавить')
+        item_3 = KeyboardButton('Изменить')
+        item_4 = KeyboardButton('Удалить')
+        keyboard.keyboard.append([item_1,item_2,item_3,item_4])
         if not arg:
-            context.bot.send_message(update.effective_chat.id, "Привет")
+            context.bot.send_message(update.effective_chat.id, "Привет", reply_markup=keyboard)
         else:
             context.bot.send_message(update.effective_chat.id, f"{' '.join(arg)}")
+
+    def show(update, context):
+        context.bot.send_message(update.effective_chat.id, f'{get_tasks}')
 
     def info(update, context):
         context.bot.send_message(update.effective_chat.id, "Меня создала компания GB!")
@@ -30,11 +41,13 @@ def tasks_bot(token):
         context.bot.send_message(update.effective_chat.id, f'Шо сказал, не пойму')
 
     start_handler = CommandHandler('start', start)
+    show_handler = CommandHandler('show', show)
     info_handler = CommandHandler('info', info)
     message_handler = MessageHandler(Filters.text, message)
     unknown_handler = MessageHandler(Filters.command, unknown)  # /game
 
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(show_handler)
     dispatcher.add_handler(info_handler)
     dispatcher.add_handler(unknown_handler)
     dispatcher.add_handler(message_handler)
